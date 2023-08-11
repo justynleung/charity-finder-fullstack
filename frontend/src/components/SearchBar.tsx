44fe38a399b6cfbf49c7b327115336285dab04dc
justynleung <justyn@justyns-mbp.home>
2023-08-11 15:47:51 -0400

    remove home nav in homepage

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -19,9 +19,6 @@ const SearchBar: React.FC<Props> = ({ setFilter }) => {
             className="flex flex-row justify-center mt-32 mb-6 w-full text-[--color-text]"
             onKeyDown={handleClick}
         >
-            <NavLink to="/">
-                Back to home
-            </NavLink>
             <div className="w-2/6 mx-4">
                 <input onChange={handleChange} type="text" placeholder="" className="w-8/12 mx-4 bg-[--color-bg] rounded-sm border-2  border-transparent focus:outline-none focus:border-2 focus:border-[--color-blue]" />
                 <button className="border border-[--color-gray-5] bg-[--color-gray-4] text-[--color-gray-0] rounded px-2" onClick={handleClick}>Search</button>
